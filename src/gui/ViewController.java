cd844456adf061ba70f1f3ccab097de265f9bfd8
marcoscaldas <donarumo@hotmail.com>
2020-09-10 17:19:47 -0300

    Calculator limited

---
+++ src/gui/ViewController.java
@@ -0,0 +1,62 @@
+package gui;
+
+import java.net.URL;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import gui.util.Alerts;
+import gui.util.Constraints;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+
+
+public class ViewController implements Initializable {
+	
+	@FXML
+	private TextField txtNumber1;
+	
+	@FXML
+	private TextField txtNumber2;
+	
+	@FXML
+	private Label labelResult;
+	
+	@FXML
+	private Button btSum;
+	
+	
+	
+	
+	@FXML
+	public void onBtSumAction () {
+		Locale.setDefault(Locale.US);
+		try {
+			double number1 = Double.parseDouble(txtNumber1.getText());
+			double number2 = Double.parseDouble(txtNumber2.getText());
+			double sum = number1 + number2;
+			
+			labelResult.setText(String.format("%.2f", sum));
+		    }
+		catch(NumberFormatException e){
+			Alerts.showAlert("Error", null, e.getMessage(), AlertType.ERROR);
+		}
+	}
+
+
+
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		Constraints.setTextFieldDouble(txtNumber1);
+		Constraints.setTextFieldDouble(txtNumber2);
+		Constraints.setTextFieldMaxLength(txtNumber1, 5);
+		Constraints.setTextFieldMaxLength(txtNumber2, 5);
+		
+	}
+	
+
+}
